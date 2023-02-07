b5e51775ef33481adb87ec27b5a78cfe3611942c
iamjinseo <58293743+iamjinseo@users.noreply.github.com>
2023-02-07 17:45:14 +0900

    SWEA 1289 원재의 메모리 복구학

--- Baekjoon/src/silver2/B1012_유기농배추.java
+++ Baekjoon/src/silver2/B1012_유기농배추.java
@@ -7,17 +7,10 @@ import java.util.StringTokenizer;
 
 public class B1012_유기농배추 {
 
-	/* 맵을 순회하면서 1을 하나 발견하면 인접한 1을 모두 탐색하여 0으로 만든 후 결과에 1더함
-	 * int map = 100 100
-	 *  while true
-	 *   0,0에서 탐색 시작
-	 *   1을 감지함.
-	 *     지렁이 += 1 
-	 *     상하좌우로 움직이는 반복문 시작
-	 * 	     1에서 상하좌우로 움직임 만약 1나옴
-	 *  	 => 그자리로 옮기기
-	 *       => 0으로 만들기
-	 *       1이 한번도 안나옴? => 0,1탐색 ㄱㄱ...
+	/*
+	 * 맵을 순회하면서 1을 하나 발견하면 인접한 1을 모두 탐색하여 0으로 만든 후 결과에 1더함 int map = 100 100 while
+	 * true 0,0에서 탐색 시작 1을 감지함. 지렁이 += 1 상하좌우로 움직이는 반복문 시작 1에서 상하좌우로 움직임 만약 1나옴 =>
+	 * 그자리로 옮기기 => 0으로 만들기 1이 한번도 안나옴? => 0,1탐색 ㄱㄱ...
 	 */
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -44,25 +37,32 @@ public class B1012_유기농배추 {
 
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < M; j++) {
-					if (map[i][j] == 1) { //1감지
+					if (map[i][j] == 1) { // 1감지
 						res++;
-						for (int n = 0; n < 4; n++) {
+						while (true) { // 0만 있을때까지 무한 사방탐색
+							int n = 0; // 탐색 방향
+
+							// 한방향으로 계속 탐색
 							int ni = i + di[n];
 							int nj = j + dj[n];
-							if(ni>=0 && ni<N && nj>=0 && nj<M && map[ni][nj]==1) {
-								i = ni;
-								j = nj; 
-								map[i][j] = 0;
+							if (ni >= 0 && ni < N && nj >= 0 && nj < M) { // 범위내
+								if (map[ni][nj] == 1) {
+									i = ni;
+									j = nj; // 자리옮기기
+									map[i][j] = 0; // 방문처리
+									continue; // 계속 탐색
+								}
 							}
-						} //사방탐색 끝
-						
-					} //1 감지 끝
-				} //맵 탐색 끝
+							n = (n + 1) % 4;
+						} // 사방탐색 끝
+
+					} // 1 감지 끝
+				} // 맵 탐색 끝
 				System.out.println(res);
 			}
-				
-		} //테스트케이스 끝
-		
+
+		} // 테스트케이스 끝
+
 	}
 
 }
