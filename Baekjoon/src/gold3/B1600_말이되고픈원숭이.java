1627174096ad19bb23a137a624b0c4d7f569b42b
iamjinseo <58293743+iamjinseo@users.noreply.github.com>
2023-03-30 13:59:24 +0900

    0330

--- Baekjoon/src/gold3/B1600_말이되고픈원숭이.java
+++ Baekjoon/src/gold3/B1600_말이되고픈원숭이.java
@@ -35,58 +35,83 @@ public class B1600_말이되고픈원숭이 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		W = Integer.parseInt(st.nextToken());
 		H = Integer.parseInt(st.nextToken());
+		map = new int[H][W];
+		System.out.println("H: " + H + "W: " + W);
 
 		for (int i = 0; i < H; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < W; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 
-				if (map[i][j] == 1) {
+				if (map[i][j] == 1) { // 1을 -1로
 					map[i][j] = -1;
 				}
 			}
 		} // end input
 
 		go(new Pair(0, 0));
+		if (map[H - 1][W - 1] == 0) {
+			System.out.println(-1);
+		} else {
+			System.out.println(map[H - 1][W - 1]);
+		}
 	}
 
 	static void go(Pair p) {
 		Queue<Pair> q = new ArrayDeque<Pair>();
 		q.add(p);
-		
+
 		while (!q.isEmpty()) {
 			Pair polled = q.poll();
+			System.out.println("polled: " + polled);
 			int i = polled.i;
 			int j = polled.j;
-			
+
 			// 나이트스텝으로 이동 가능한지 검사
 			Pair knightPair = isKnight(i, j);
-			
+
 			// 나이트스텝으로 이동 가능
 			if (knightPair != null) {
 				k -= 1;
 				int ni = knightPair.i;
 				int nj = knightPair.j;
 				map[ni][nj] = map[i][j] + 1;
-				if(ni==H-1 && nj==W-1) return;
+				System.out.println("나이트스텝 다음위치는 " + ni + ", " + nj + "이고 값은 " + map[ni][nj]);
+				if (ni == H - 1 && nj == W - 1) {
+					System.out.println("도달");
+					return;
+				}
 				q.offer(new Pair(ni, nj));
-			} 
+			}
 			// 나이트스텝으로 이동 불가
 			else {
 				for (int n = 0; n < 4; n++) {
 					int ni = i + di[n];
 					int nj = j + dj[n];
-					if(ni==H-1 && nj==W-1) return;
-					if (isValidScope(ni, nj))
-						map[ni][nj] = map[i][j] + 1;
+					if (ni == H - 1 && nj == W - 1) {
+						System.out.println("도달");
+						return;
+					}
+					if (isValidScope(ni, nj)) {
+						if (map[ni][nj] > 0) {
+							map[ni][nj] = Math.min(map[i][j] + 1, map[ni][nj]);
+							System.out.println("스텝 다음위치는 " + ni + ", " + nj + "이고 값은 " + map[ni][nj]);
+							q.offer(new Pair(ni, nj));
+						} else if (map[ni][nj] == 0) {
+							map[ni][nj] = map[i][j] + 1;
+							System.out.println("스텝 다음위치는 " + ni + ", " + nj + "이고 값은 " + map[ni][nj]);
+							q.offer(new Pair(ni, nj));
+						}
+					}
 				}
 			}
 		}
 	}
 
 	static Pair isKnight(int i, int j) {
-		if(k<=0) return null;
-		
+		if (k <= 0)
+			return null;
+
 		// 오른쪽으로가서 아래로가는거
 		int ni = i + knightStep[0][0];
 		int nj = j + knightStep[0][1];
@@ -94,6 +119,7 @@ public class B1600_말이되고픈원숭이 {
 		if (!isValidScope(ni, nj))
 			return null;
 		if (map[ni][nj] == 0) {
+			System.out.println(i + ", " + j + "에서 오른쪽 아래로 나이트스텝 가능.");
 			return new Pair(ni, nj);
 		}
 		// 아래로가서 오른쪽으로 가는거
@@ -103,8 +129,10 @@ public class B1600_말이되고픈원숭이 {
 
 			if (!isValidScope(ni, nj))
 				return null;
-			if (map[ni][nj] == 0)
+			if (map[ni][nj] == 0) {
+				System.out.println(i + ", " + j + "에서 아래로가서 오른쪽으로 나이트스텝 가능.");
 				return new Pair(ni, nj);
+			}
 		}
 		return null;
 	}
