08c1163fd43b580c3cb36a7b0c0aceb91aed90eb
jinseo <ljs20171103@gmail.com>
2023-04-06 17:55:51 +0900

    0406

---
+++ Baekjoon/src/gold3/B16236.java
@@ -0,0 +1,160 @@
+package gold3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B16236 {
+	static class Shark {
+		int i, j, time, weight;
+
+		public Shark(int i, int j, int time, int weight) {
+			super();
+			this.i = i;
+			this.j = j;
+			this.time = time;
+			this.weight = weight;
+		}
+
+		@Override
+		public String toString() {
+			return "Shark [i=" + i + ", j=" + j + ", time=" + time + ", weight=" + weight + "]";
+		}
+
+	}
+
+	static int[][] map;
+	static int N, res;
+	static int[] di = { -1, 1, 0, 0 };
+	static int[] dj = { 0, 0, -1, 1 };
+	static Shark shark; // 상어의 위치와 상태
+	static boolean findFood; // 음식을 찾았는지
+	static Queue<Shark> foods = new LinkedList<>();
+	static int eatCnt; // 상어가 먹은 물고기 수
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				
+				if(map[i][j]==9) {
+					shark = new Shark(i, j, 0, 2);
+					map[i][j] = 0;
+				}
+			}
+		} // end input
+		
+		do {
+			findFood = false;
+			System.out.println("=======start bfs=====");
+			bfs(shark); // 상어가 이동할 수 있는지 볼 거임
+		}while(findFood); // 먹이를 찾을 수 있는 동안
+		
+		System.out.println(res);
+	}
+	
+	static void bfs(Shark s) {
+		boolean[][] visit = new boolean[N][N]; 
+		Queue<Shark> q = new LinkedList<>();
+		q.offer(s);
+		visit[s.i][s.j] = true;
+		
+		while(!q.isEmpty()) {
+			Shark now = q.poll();
+			for (int n = 0; n < 4; n++) {
+				int ni = now.i + di[n];
+				int nj = now.j + dj[n];
+				
+				if(ni<0 || ni>=N || nj<0 || nj>=N || visit[ni][nj]) continue;
+				
+				if(map[ni][nj] == 0 || map[ni][nj] == now.weight) { // 이동만 할 수 있음
+					System.out.printf("[%d][%d](%d)로 이동. time: %d\n", ni, nj, map[ni][nj], now.time+1);
+					q.offer(new Shark(ni, nj, now.time+1, now.weight));
+					visit[ni][nj] = true;
+				}
+				else if(map[ni][nj] < now.weight) { // 먹을 수 있음
+					System.out.printf("[%d][%d](%d)로 먹으러감. time: %d\n", ni, nj, map[ni][nj], now.time+1);
+					foods.add(new Shark(ni, nj, now.time+1, now.weight));
+					
+					// 다른 사방에도 먹을 게 있을지도
+//					for (int n2 = 0; n2 < 4; n2++) {
+//						
+//						if(n2==n) continue;
+//						
+//						int ni2 = now.i + di[n2];
+//						int nj2 = now.j + dj[n2];
+//						
+//						if(ni2<0 || ni2>=N || nj2<0 || nj2>=N || visit[ni2][nj2]) continue;
+//						
+//						if(map[ni2][nj2] < now.weight && map[ni2][nj2]!=0) {
+//							System.out.printf("[%d][%d](%d)로도 먹으러감ㅋ. time: %d\n", ni2, nj2, map[ni2][nj2], now.time+1);
+//							foods.add(new Shark(ni2, nj2, now.time+1, now.weight));
+//						}
+//					}
+					findFood = true;
+					break;
+				}
+			}
+		}
+		if(!findFood) // 위에서 음식 못찾았으면 망함 
+			return; 
+		
+		// 가까운 음식을 찾았어도 위에있고 왼쪽에 있는 걸 고를 수 있음 
+		visit = new boolean[N][N]; 
+		int size = q.size();
+		while(size-->0) {
+			Shark now = q.poll();
+			for (int n = 0; n < 4; n++) {
+				int ni = now.i + di[n];
+				int nj = now.j + dj[n];
+				
+				if(ni<0 || ni>=N || nj<0 || nj>=N || visit[ni][nj]) continue;
+				
+				if(map[ni][nj] == 0 || map[ni][nj] == now.weight) { // 이동만 할 수 있음
+					q.offer(new Shark(ni, nj, now.time+1, now.weight));
+					visit[ni][nj] = true;
+				}
+				else if(map[ni][nj] < now.weight)  // 먹을 수 있음
+					foods.add(new Shark(ni, nj, now.time+1, now.weight));
+			}
+		}
+		
+		finalLoc(); // 최종적으로 상어의 상태 변경
+	}
+	
+	// 같은 거리 중 더 위에 있고 왼쪽에 있는 것
+	static void finalLoc() {
+		Shark loc = foods.poll();
+		while(!foods.isEmpty()) {
+			Shark s = foods.poll();
+			if(s.i < loc.i) { // 위에있는거 선택
+				if(s.time <= loc.time ) 
+					loc = s;
+			}else if(s.i == loc.i) {
+				if(s.j < loc.j && s.time <= loc.time ) 
+					loc = s;
+			}
+		}
+		
+		// 결국 먹이가 골라지고 상어가 그쪽으로 이동
+		res = loc.time;
+		eatCnt++;
+		if(eatCnt == shark.weight) {
+			loc.weight++;
+			eatCnt=0;
+		}
+		shark = loc;
+		map[loc.i][loc.j] = 0;
+		System.out.printf("결국 상어는 [%d][%d]에 있고 지금까지 %d개 먹어서 무게는 %d이고 걸린 시간은 %d임\n", shark.i, shark.j, eatCnt, shark.weight, shark.time);
+	}
+}
--- Baekjoon/src/gold3/B1941_소문난칠공주.java
+++ Baekjoon/src/gold3/B1941_소문난칠공주.java
@@ -1,8 +1,9 @@
 package gold3;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class B1941_소문난칠공주 {
 	static class Pair {
@@ -18,29 +19,78 @@ public class B1941_소문난칠공주 {
 	static char[][] map = new char[5][5];
 	static int[] di = { -1, 1, 0, 0 };
 	static int[] dj = { 0, 0, 1, -1 };
-	static boolean[][] visit = new boolean[5][5];
 	static int res;
+	static boolean[] used;
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		Scanner sc = new Scanner(System.in);
+		used = new boolean[25];
 		for (int i = 0; i < 5; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < 5; j++) {
-				map[i][j] = str.charAt(j);
-			}
-		} // end input
-
-		combi();
+			map[i] = sc.next().toCharArray();
+		}
+		
+		combi(0, 0);
 		System.out.println(res);
 	}
-	
-	
 
-	// 범위 밖이거나 방문했거나 돌이면 안됨
-	private static boolean isValid(int ni, int nj) {
-		if (ni < 0 || ni >= 5 || nj < 0 || nj >= 5 || visit[ni][nj])
-			return false;
-		return true;
+	static void combi(int idx, int cnt) {
+		if(cnt==7) { // 아무거나 7칸 뽑음
+			// 1. 7명 다 뿥어있는지 체크
+			boolean isSeven = false;
+			for (int i = 0; i < 25; i++) {
+				if(used[i]) {
+					isSeven = bfs(i);
+					break;
+				}
+			}
+			
+			// 2. S가 4개 이상인지 체크
+			int scnt = 0; 
+			for(int i=0; i<25; i++) {
+				if(used[i] && map[i/5][i%5]=='S')
+					scnt++;
+			}
+			
+			if(isSeven && scnt>=4) // 뽑은 7개가 모두 붙어있고 S가 4개 이상이면 경우의수 카운트
+				res++;
+			
+			return; 
+		}
+		if(idx == 25) return;
+		
+		used[idx] = true;
+		combi(idx + 1, cnt + 1);
+		used[idx] = false;
+		combi(idx + 1, cnt);
+	}
+	
+	static boolean bfs(int start) {
+		int si = start/5;
+		int sj = start%5;
+		Queue<Pair> q = new LinkedList<>();
+		boolean[][] visit = new boolean[5][5];
+		
+		q.offer(new Pair(si, sj));
+		visit[si][sj] = true;
+		
+		int cnt=0;
+		while(!q.isEmpty()) {
+			Pair now = q.poll();
+			cnt++; // 하나 꺼낼 때마다 카운트+1
+			
+			if(cnt==7) return true; // 칠공주가 싹다 붙어있음
+			
+			for (int n = 0; n < 4; n++) {
+				int ni = now.i + di[n];
+				int nj = now.j + dj[n];
+
+				// 상하좌우에 칠공주가 붙어있으면
+				if(ni>=0 && ni<5 && nj>=0 && nj<5 && used[ni*5+nj] && !visit[ni][nj]) {
+					q.offer(new Pair(ni, nj));
+					visit[ni][nj] = true;
+				}
+			}
+		}
+		return false;
 	}
 }
